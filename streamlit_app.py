854e1ccdaa2ce268ca6febee3326b77895706ec3
Nette018 <140135940+Nette018@users.noreply.github.com>
2023-07-21 13:10:49 +0800

    Update streamlit_app.py
    Added Menu List

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit.subheader('Omega 3 & Blueberry Oatmeal')
+streamlit.subheader('Kale, Spinach & Rocket Smoothie')
+streamlit.subheader('Hard-Boiled Free-Range Egg')
